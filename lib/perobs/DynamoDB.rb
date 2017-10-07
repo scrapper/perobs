6bce839893b28aacf1deb28cf3e196ed0b182a4e
Chris Schlaeger <chris@linux.com>
2017-10-07 21:29:32 +0200

    New: Support for aws-sdk version 3.

--- lib/perobs/DynamoDB.rb
+++ lib/perobs/DynamoDB.rb
@@ -25,7 +25,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-require 'aws-sdk-core'
+require 'aws-sdk'
 
 require 'perobs/DataBase'
 require 'perobs/BTreeBlob'
