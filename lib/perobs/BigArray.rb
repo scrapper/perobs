bb56066b1f51d51bbec3bb922b39baf4197c69c5
Chris Schlaeger <chris@linux.com>
2019-04-22 16:44:45 +0200

    Fix: Make break work for BigArray::each

--- lib/perobs/BigArray.rb
+++ lib/perobs/BigArray.rb
@@ -196,7 +196,7 @@ module PEROBS
     def each(&block)
       node = @first_leaf
       while node
-        node.each(&block)
+        break unless node.each(&block)
         node = node.next_sibling
       end
     end
@@ -207,7 +207,7 @@ module PEROBS
     def reverse_each(&block)
       node = @last_leaf
       while node
-        node.reverse_each(&block)
+        break unless node.reverse_each(&block)
         node = node.prev_sibling
       end
     end
